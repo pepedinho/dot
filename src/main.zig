c6aedab22941c860bda2306b05a42fc75913c327
pepedinho <2spii94@gmail.com>
2026-03-20 19:26:42 +0100

    [editor/ui] add display mode funtion in ui

--- src/main.zig
+++ src/main.zig
@@ -30,7 +30,7 @@ pub fn main() !void {
 
     while (dot.is_running) {
         if (dot.needs_redraw) {
-            try ui.refreshScreen(stdout, &dot.buf);
+            try ui.refreshScreen(stdout, &dot);
         } else {
             try ui.updateCurrentLine(stdout, &dot.buf);
         }
@@ -75,67 +75,7 @@ pub fn main() !void {
         if (action) |a| {
             try dot.execute(a);
         }
+        try dot.win.updateSize();
     }
     try stdout.writeAll("\x1b[2J\x1b[H");
 }
-
-//
-// var dot = try DotEditor.init(allocator);
-// defer dot.deinit();
-//
-// while (dot.is_running) {
-//     // --- VUE ---
-//     if (dot.needs_redraw) {
-//         try ui.refreshScreen(stdout, &dot.buf);
-//     } else {
-//         try ui.updateCurrentLine(stdout, &dot.buf);
-//     }
-//     // TODO: Afficher le mode actuel dans un coin de l'écran (ex: "-- INSERT --")
-//     try stdout_writer.flush();
-//
-//     // --- INPUT ---
-//     const key = try keyboard.readKey();
-//     if (key == .none) continue;
-//
-//     // --- MAPPING (Clavier -> API) ---
-//     var action_to_execute: ?Action = null;
-//
-//     switch (dot.mode) {
-//         .Normal => {
-//             switch (key) {
-//                 .ascii => |c| {
-//                     if (c == 'i') action_to_execute = .{ .SetMode = .Insert };
-//                     if (c == 'h') action_to_execute = .MoveLeft;
-//                     if (c == 'j') action_to_execute = .MoveDown;
-//                     if (c == 'k') action_to_execute = .MoveUp;
-//                     if (c == 'l') action_to_execute = .MoveRight;
-//                     if (c == 'x') action_to_execute = .DeleteChar;
-//                 },
-//                 .escape => {
-//                     // Pour quitter temporairement sans faire de mode :q
-//                     action_to_execute = .Quit;
-//                 },
-//                 else => {},
-//             }
-//         },
-//         .Insert => {
-//             switch (key) {
-//                 .escape => action_to_execute = .{ .SetMode = .Normal },
-//                 .ascii => |c| action_to_execute = .{ .InsertChar = c },
-//                 .enter => action_to_execute = .InsertNewLine,
-//                 .backspace => action_to_execute = .DeleteChar,
-//                 .left => action_to_execute = .MoveLeft,
-//                 .right => action_to_execute = .MoveRight,
-//                 else => {},
-//             }
-//         },
-//         .Command => {
-//             // Pour plus tard, quand on tapera ":w" ou ":q"
-//         }
-//     }
-//
-//     // --- DISPATCH ---
-//     if (action_to_execute) |action| {
-//         try dot.execute(action);
-//     }
-// }
