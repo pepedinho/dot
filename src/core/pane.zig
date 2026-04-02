969053e6dff3014e14a7bfdd10320c590862c0a6
pepedinho <2spii94@gmail.com>
2026-04-02 16:17:12 +0200

    [doc] commenting pane module

--- src/core/pane.zig
+++ src/core/pane.zig
@@ -1,14 +1,33 @@
 const gap = @import("gap.zig");
 
+/// A `View` represent a rectangular viewport on the terminal screen.
+/// It act as a 'camera' looking into a specific `GapBuffer`.
+///
+/// While the `GapBuffer` strictly holds the raw text data, the `View` manages
+/// the visual representation: screen coordinate (x, y), dimension (width, height),
+/// scroll offset, and UI states (read-only, dirty, flags).
+///
+/// Multiple views can exist simulataneously (e.g., via split panes),
+/// pointing to different buffers or even different parts of the same buffer.
 pub const View = struct {
+    /// Absolute terminal column position.
     x: u16,
+    /// Absolute terminal row position.
     y: u16,
+    /// Visible width of the viewport in columns.
     width: u16,
+    /// Visible height of the viewport in rows.
     height: u16,
+
+    ///The vertical scroll position (number of logical lines hidden above).
     row_offset: usize = 0,
+    /// The horizontal scroll position (number of logical columns hidden to the left).
     col_offset: usize = 0,
+    /// Pointer to the underlying text data.
     buf: *gap.GapBuffer,
+    /// If true, blocks any Insert or Delete actions from the user.
     is_readonly: bool = false,
+    /// If true, the UI engine will redraw this specific viewport on the next tick.
     is_dirty: bool = false,
 
     pub fn scroll(self: *View) bool {
